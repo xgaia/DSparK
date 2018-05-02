5556f342ed00ae45fd1443e9720fd850ba6e2f96
xgaia <xavier.garnier@irisa.fr>
2018-05-02 15:18:35 +0200

    df

--- src/main/scala/Dspark.scala
+++ src/main/scala/Dspark.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.mllib.rdd.RDDFunctions._
+import org.apache.spark.sql.functions.udf
 //import scala.util.{Failure, Success, Try}
 import fr.inra.lipm.general.paramparser.ArgParser
 
@@ -11,6 +13,14 @@ object Dspark {
     // Spark configuration
     val conf = new SparkConf().setAppName("DSparK")
     val sc = new SparkContext(conf)
+
+    val spark = SparkSession
+      .builder()
+      .appName("DSparK")
+      .getOrCreate()
+
+    import spark.implicits._
+
 
     // Param parser
     val parser = new ArgParser()
@@ -43,7 +53,7 @@ object Dspark {
 
     //Broadcast variables on all nodes
     val broadcastedKmerSize = sc.broadcast(kmerSize)
-    val broadcastedHalphKmerSize = sc.broadcast((kmerSize / 2 ) + 1)
+    val broadcastedHalphKmerSize = sc.broadcast((kmerSize / 2) + 1)
     val broadcastedAbundanceMax = sc.broadcast(abundanceMax)
     val broadcastedAbundanceMin = sc.broadcast(abundanceMin)
     val broadcastedSortOrder = sc.broadcast(sortOrder)
@@ -51,61 +61,56 @@ object Dspark {
 
     // main --------------------------
     val reads = inputType match {
-      case "fasta" => sc.textFile(input).sliding(2, 2).map{case Array(id, seq) => seq}
-      case "fastq" => sc.textFile(input).sliding(4, 4).map{case Array(id, seq, _, qual) => seq}
+      case "fasta" => sc.textFile(input).sliding(2, 2).map { case Array(id, seq) => seq }
+      case "fastq" => sc.textFile(input).sliding(4, 4).map { case Array(id, seq, _, qual) => seq }
     }
 
-    val kmers = reads.flatMap(read => read.sliding(kmerSize, 1))
-    val kmersWithoutN = kmers.filter(kmer => !kmer.contains("N"))
+    // Df of kmers without N
+    val kmers = reads.flatMap(read => read.sliding(kmerSize, 1)).toDF("kmer").filter(!$"kmer".contains("N"))
 
-    def getCanonicalIterator(iterKmer: Iterator[String]): Iterator[String] = {
 
-      def isCanonical(kmer: String, reverseKmer: String): Boolean = {
+    def isCanonical(kmer: String, reverseKmer: String): Boolean = {
 
-        val sub = broadcastedSortOrder.value(broadcastedBaseComplement.value(reverseKmer.head)) - broadcastedSortOrder.value(kmer.head)
+      val sub = broadcastedSortOrder.value(broadcastedBaseComplement.value(reverseKmer.head)) - broadcastedSortOrder.value(kmer.head)
 
-        sub match {
-          case 0 => isCanonical(kmer.tail, reverseKmer.tail)
-          case a if a > 0 => true
-          case a if a < 0 => false
-        }
+      sub match {
+        case 0 => isCanonical(kmer.tail, reverseKmer.tail)
+        case a if a > 0 => true
+        case a if a < 0 => false
       }
-
-      def getCanonical(kmer: String): String = {
-        // reverse seq
-        val revKmer = kmer.reverse
-        if (isCanonical(kmer, revKmer)) {
-          kmer
-        } else {
-          // complement
-          revKmer.map(broadcastedBaseComplement.value(_))
-        }
-      }
-
-      iterKmer.map(getCanonical)
-
     }
 
-    val canonicalKmers = kmersWithoutN.mapPartitions(getCanonicalIterator)
+    def getCanonical(kmer: String): String = {
+      // reverse seq
+      val revKmer = kmer.reverse
+      if (isCanonical(kmer, revKmer)) {
+        kmer
+      } else {
+        // complement
+        revKmer.map(broadcastedBaseComplement.value(_))
+      }
+    }
+
+    def gc: (String => String) = { str => getCanonical(str) }
+
+    val getCanonicalUDF = udf(gc)
+
+
+    // get canonical
+    val canonicalKmers = kmers.withColumn("canonical kmer", getCanonicalUDF(kmers("kmer"))).select("canonical kmer")
 
     // count kmers
-    val countedKmers = canonicalKmers.map((_, 1)).reduceByKey(_ + _)
+    val result = canonicalKmers.groupBy("canonical kmer").count.filter($"count" >= abundanceMin).filter($"count" <= abundanceMax)
 
-    // filter on abundance
-    val filteredKmers = countedKmers.filter(kmer_tpl => kmer_tpl._2 >= broadcastedAbundanceMin.value && kmer_tpl._2 <= broadcastedAbundanceMax.value)
 
     // Sort
     val sortedKmers = sorted match {
-      case 0 => filteredKmers
-      case _ => filteredKmers.sortByKey()
+      case 0 => result
+      case _ => result.sort("canonical kmer")
     }
 
-    // format
-    val formatedKmers = format match {
-      case 0 => sortedKmers
-      case _ => sortedKmers.map(x => x.toString().replace("(", "").replace(")", "").replace(",", " "))
-    }
 
-    formatedKmers.saveAsTextFile(output)
+    sortedKmers.write.format("csv").save(output)
+
   }
 }
